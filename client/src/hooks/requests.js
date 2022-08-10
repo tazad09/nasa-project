58214623f9071321d3560dfb12e39d9a15c97373
Tasnim <tasnimazad17@gmail.com>
2022-08-09 21:25:47 -0400

    added functionalities

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,11 +1,16 @@
+const API_URL = "http://localhost:8000";
+
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
   // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json()
 }
 
 async function httpGetLaunches() {
+
   // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
+
 }
 
 async function httpSubmitLaunch(launch) {
---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const app = require ('./app.js')
+const {loadPlanetsData} = require('./models/planets.model')
+
+
+const PORT = 8000;
+
+const server = http.createServer(app);
+
+
+async function startServer () {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
+}
+
+
+startServer()
