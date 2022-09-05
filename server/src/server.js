f04b3b544e0e773c693933f964d4b58f28e40df6
Tasnim <tasnimazad17@gmail.com>
2022-09-05 15:10:58 -0400

    .

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-
 //calling loadPlanets here before we start listening to server. The reason for this is when we get request from front to get all the planets, the data may not be fully ready. We are using async because loadPlanetsData is a promise.
 async function startServer () {
   await mongoConnect()
